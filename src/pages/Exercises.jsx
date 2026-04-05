0a2bcb3735e7a5161557aea2fbc56325f85ab0d6
Patrik <p.pavlenda@gmail.com>
2026-04-05 22:46:40 +0200

    Hide exercise level label in web settings cards only

--- src/pages/Exercises.jsx
+++ src/pages/Exercises.jsx
@@ -1644,7 +1644,7 @@ function Exercises({ webSettingsSection = '' }) {
                 {/* Karty Počet hráčov a Intenzita byly odstraněny na přání uživatele */}
 
                 <div className="exercise-db-card-rating" aria-label="Hodnotenie cvičenia">
-                  <small>Úroveň cvičenia</small>
+                  {!isEmbeddedWebSettingsView ? <small>Úroveň cvičenia</small> : null}
                   <div className="exercise-db-card-rating-stars" role="group" aria-label={`Hodnotenie cvičenia ${item.name}`}>
                     {[1, 2, 3, 4, 5].map((starValue) => (
                       <button
